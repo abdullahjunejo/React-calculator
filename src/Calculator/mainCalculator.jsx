02cbda64ab77694e098b7e455ae685a473b9c9f5
Abdullah junejo <126189271+abdullahjunejo@users.noreply.github.com>
2023-06-25 17:46:21 +0500

    Add files via upload

---
+++ src/Calculator/mainCalculator.jsx
@@ -0,0 +1,14 @@
+import Buttons from "./buttons";
+import "./Allcss.css";
+
+
+function Calculator() {
+  return (
+    <div className="mainBody border-current border-2 h-96 w-72 rounded-md bg-slate-800">
+      
+
+      <Buttons />
+    </div>
+  );
+}
+export default Calculator;
